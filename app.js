d9cfe0c8e458cf2e372613ee26b49f6ff0432f07
Matt Martinez <mattmartinez21@gmail.com>
2020-01-27 23:45:58 -0500

    got date and time values

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+let fullDate = new Date();
+let hours = fullDate.getHours();
+let minutes = fullDate.getMinutes();
+let seconds = fullDate.getSeconds();
 
+const hours_div = document.querySelector("#hour");
+const minutes_div = document.querySelector("#minute");
+const seconds_div = document.querySelector("#second");
+
+hours_div.innerHTML = hours;
+minutes_div.innerHTML = minutes;
+seconds_div.innerHTML = seconds;
