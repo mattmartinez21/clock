283832294d3aa2bc5b6647860452eb7837a67c3d
Matt Martinez <mattmartinez21@gmail.com>
2020-01-28 19:31:42 -0500

    fixed seconds casing

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ function time() {
   const minutes_div = document.querySelector("#minute");
   const seconds_div = document.querySelector("#second");
 
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
   hours_div.innerHTML = `${hours}:`;
   minutes_div.innerHTML = `${minutes}:`;
   seconds_div.innerHTML = seconds;
